faf82ffe0bb9f05776b16101fee36c7bf6476946
Yahton <yahton@duck.com>
2025-06-26 20:52:41 +0300

    proper logs

--- include/logs.h
+++ include/logs.h
@@ -9,10 +9,4 @@ enum LoggingLevel {
 
 bool logging_init(void);
 void logging_deinit(void);
-void logging_log(enum LoggingLevel ll, const char *text);
-
-/*
-void info(const char *text);
-void warning(const char *text);
-void error(const char *text);
-*/
+void logging_log(enum LoggingLevel ll, const char *fmt, ...);
--- src/common/logs.c
+++ src/common/logs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <time.h>
 #include <sys/stat.h>
 
@@ -10,10 +11,10 @@
 #define lmkdir(dir) mkdir(dir, 0755)
 #endif // _WIN32
 
-#define DEBUG_FORMAT "[%s] [DEBUG]: %s\n"
-#define INFO_FORMAT "[%s] [INFO]: %s\n"
-#define WARNING_FORMAT "[%s] [WARNING]: %s\n"
-#define ERROR_FORMAT "[%s] [ERROR]: %s\n"
+#define DEBUG_FORMAT "[%s] [DEBUG]: %s "
+#define INFO_FORMAT "[%s] [INFO]: %s "
+#define WARNING_FORMAT "[%s] [WARNING]: %s "
+#define ERROR_FORMAT "[%s] [ERROR]: %s "
 
 const char *LOGGING_MESSAGES[] = {
   DEBUG_FORMAT,
@@ -46,9 +47,22 @@ void logging_deinit(void) {
   fclose(f);
 }
 
-void logging_log(enum LoggingLevel ll, const char *text) {
+void logging_log(enum LoggingLevel ll, const char *fmt, ...) {
   char buffer[26];
   formatted_time(buffer);
-  fprintf(f, LOGGING_MESSAGES[ll], buffer, text);
-  printf(LOGGING_MESSAGES[ll], buffer, text);
+
+  va_list args;
+
+  va_start(args, fmt);
+  fprintf(f, LOGGING_MESSAGES[ll], buffer);
+  vfprintf(f, fmt, args);
+  fprintf(f, "\n");
+  va_end(args);
+
+  va_start(args, fmt);
+  printf(LOGGING_MESSAGES[ll], buffer);
+  vprintf(fmt, args);
+  printf("\n");
+
+  va_end(args);
 }
